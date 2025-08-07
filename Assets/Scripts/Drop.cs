f840f0462b7376d2a924d2923088edb7cde6a5c7
ByunDongHyun <ehdgusqus55@gamil.com>
2025-08-07 16:18:52 +0900

    update 25.8.7

---
+++ Assets/Scripts/Drop.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Drop : MonoBehaviour
+{
+    [SerializeField] private float customGravity = -30f;
+    MeshRenderer mr;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        if (gameObject != null)
+        {
+            Debug.Log("gameObject.SetActive(false)");
+            gameObject.SetActive(false);    
+        }
+    }
+
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
+
+        rb.useGravity = false;  // 기본 중력 비활성화
+        mr.enabled = true;      // MeshRenderer 활성화
+    }
+
+    void FixedUpdate()
+    {
+        // ForceMode.Acceleration -> 질량(Mass)을 무시하고, Rigidbody에 지속적인 가속도를 적용
+        rb.AddForce(Vector3.up * customGravity, ForceMode.Acceleration);
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] GameObject player;     // player -> Drop_Rock
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // Debug.Log("Drop_Rock SetActive");
+            player.SetActive(true);
+        }
+    }
+}
