95d74f4f4e46cc5bf8ca502a0682ee500eb529d9
ByunDongHyun <ehdgusqus55@gamil.com>
2025-08-11 14:21:20 +0900

    update 25.8.11

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private bool isMovingUpwards; // true면 위로, false면 아래로 이동 
+    [SerializeField] private float moveDistance = 90f;
+    [SerializeField] private float moveSpeed = 20f;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        MoveUpDown();
+    }
+
+    void MoveUpDown()
+    {
+        float yOffset = Mathf.PingPong(Time.time * moveSpeed, moveDistance);
+
+        Vector3 direction = (isMovingUpwards) ? Vector3.up : Vector3.down;
+
+        transform.position = startPosition + direction * yOffset;
+    }
+}
